8cb3cde9fe478c61b8afb054f16f735283616b54
Christian0102 <37531538+Christian0102@users.noreply.github.com>
2020-03-26 12:42:07 +0200

    Add files via upload
    Added Database

--- database/test.sql
+++ database/test.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1
--- Время создания: Мар 09 2020 г., 15:28
--- Версия сервера: 10.4.11-MariaDB
--- Версия PHP: 7.4.3
+-- Хост: localhost
+-- Время создания: Мар 26 2020 г., 11:40
+-- Версия сервера: 10.1.37-MariaDB
+-- Версия PHP: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,6 +22,18 @@ SET time_zone = "+00:00";
 -- База данных: `test`
 --
 
+DELIMITER $$
+--
+-- Процедуры
+--
+CREATE DEFINER=`` PROCEDURE `AddGeometryColumn` (`catalog` VARCHAR(64), `t_schema` VARCHAR(64), `t_name` VARCHAR(64), `geometry_column` VARCHAR(64), `t_srid` INT)  begin
+  set @qwe= concat('ALTER TABLE ', t_schema, '.', t_name, ' ADD ', geometry_column,' GEOMETRY REF_SYSTEM_ID=', t_srid); PREPARE ls from @qwe; execute ls; deallocate prepare ls; end$$
+
+CREATE DEFINER=`` PROCEDURE `DropGeometryColumn` (`catalog` VARCHAR(64), `t_schema` VARCHAR(64), `t_name` VARCHAR(64), `geometry_column` VARCHAR(64))  begin
+  set @qwe= concat('ALTER TABLE ', t_schema, '.', t_name, ' DROP ', geometry_column); PREPARE ls from @qwe; execute ls; deallocate prepare ls; end$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -33,26 +45,44 @@ CREATE TABLE `tasks` (
   `user_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `edit_admin` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Дамп данных таблицы `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `user_name`, `email`, `description`, `status`) VALUES
-(1, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(2, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(3, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(4, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(5, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(6, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(7, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(8, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(9, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(10, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0),
-(11, 'bb0102', 'xxx@mail.com', 'xxxx', 0),
-(12, 'christian0102', 'christia0102@mail.com', 'Description of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of TasksDescription of Tasks', 0);
+INSERT INTO `tasks` (`id`, `user_name`, `email`, `description`, `status`, `edit_admin`) VALUES
+(31, 'validamina', 'mina@mail.com', '32323232323232329223372036854775807', 1, 1),
+(32, 'adminedit', 'adminedit@mail.com', '   adminedit', 0, 1),
+(33, 'iulian', 'iulian@mail.com', 'task1', 1, 1),
+(34, 'admincreat', 'create@mailc.om', 'saffsfsfsfsfsfsfsfsffs', 1, 0),
+(35, 'admin2', 'admiN@mail.com', 'fdsfsfsfsfs', 1, 0),
+(36, 'guest', 'guest@mail.com', ' fsfsfsf2', 1, 1),
+(37, 'gest1', 'gues@mail.com', '32323232', 0, 0),
+(38, 'admincreat', 'admincreate@mail.com', 'admincreate', 1, 0),
+(39, 'aaaaa', 'aaaaaa@aaaa.aaa', '       aaaaaaaaaa', 0, 1),
+(40, 'zzzzzz', 'zzzzzz@zzzzzz.zzzz', '  zzzzzzzzzzzzzzzzzzzz1111', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`) VALUES
+(1, 'admin', '123');
 
 --
 -- Индексы сохранённых таблиц
@@ -65,6 +95,12 @@ ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -72,7 +108,13 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
